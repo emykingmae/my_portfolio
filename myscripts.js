412be5abd5d97da8115d7f7b74267282c0a7a9eb
justinbthrift@gmail.com <justinthrift@Justins-MBP.home>
2020-04-16 19:56:31 -0400

    mise-updates

--- myscripts.js
+++ myscripts.js
@@ -17,7 +17,6 @@ var myDoughnutChart = new Chart(ctx, {
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-
             ],
             borderWidth: 2
         }]
